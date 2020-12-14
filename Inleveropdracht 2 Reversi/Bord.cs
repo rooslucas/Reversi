114e03c3f75291df3f4a647897b711f543ef474a
rooslucas <rooslucas@live.nl>
2020-12-14 16:10:15 +0100

    Drop files from .gitignore

--- Inleveropdracht 2 Reversi/Bord.cs
+++ Inleveropdracht 2 Reversi/Bord.cs
@@ -6,5 +6,38 @@ namespace Reversi
 {
     class Bord
     {
+        // Lengte en breedte van het bord. Zou door de gebruiker kunnen worden aangepast
+        public int Lengte;
+        public int Breedte;
+
+        // Variabele om te bepalen wie er aan de beurt is
+        public int Player = 1;
+
+        // Hier worden de berekeningen/tests mee gedaan. 
+        // Bij waarde 0 is het stuk leeg, bij waarde 1 is het stuk Rood en bij waarde 2 is het stuk blauw.
+        public int[,] Velden;
+
+        // Maakt een leeg bord met in het midden 4 stukken van beide partijen.
+        public Bord(int Lengte, int Breedte)
+        {
+            this.Lengte = Lengte;
+            this.Breedte = Breedte;
+            this.Velden = new int[Lengte, Breedte];
+            for (int i = 0; i < Lengte; i++)
+            {
+                for (int j = 0; j < Breedte; j++)
+                    Velden[i, j] = 0;
+            }
+            Velden[Lengte / 2 - 1, Breedte / 2 - 1] = 1;
+            Velden[Lengte / 2, Breedte / 2] = 1;
+            Velden[Lengte / 2, Breedte / 2 - 1] = 2;
+            Velden[Lengte / 2 - 1, Breedte / 2] = 2;
+        }
+
+        void BeurtValide()
+        {
+            // TODO kleur leeg
+            // TODO: Plek valide
+        }
     }
 }
--- Inleveropdracht 2 Reversi/Reversi.cs
+++ Inleveropdracht 2 Reversi/Reversi.cs
@@ -5,35 +5,30 @@ namespace Reversi
 {
     class Reversi : Form
     {
-        // Lengte en breedte van het bord. Zou door de gebruiker kunnen worden aangepast
-        int Lengte = 6;
-        int Breedte = 6;
-
-        // Variabele om te bepalen wie er aan de beurt is
-        int Player = 1;
         
         // Dit zijn alle buttons op het reversibord.
         Button[,] klik_veld;
+
+        // Variabele om bord in op te slaan
+        Bord bord;
         
-        // Hier worden de berekeningen/tests mee gedaan. 
-        // Bij waarde 0 is het stuk leeg, bij waarde 1 is het stuk Rood en bij waarde 2 is het stuk blauw.
-        int[,] Bord;
 
         public Reversi()
         {
-            this.ClientSize = new Size(Lengte * 60, Breedte * 60);
+            bord = new Bord(6, 6); // Initalizeer het model
+
+            this.ClientSize = new Size(bord.Lengte * 60, bord.Breedte * 60);
             this.Text = "Reversi";
-            Bord = start_bord(Lengte,Breedte);
-            this.Paint += this.genereer_klikveld;
+            genereer_klikveld();
             
         }
         //Maakt klik_veld en voegt hem aan de Form toe
-        void genereer_klikveld(object sender, PaintEventArgs pea)
+        void genereer_klikveld()
         {
-            klik_veld = new Button[Lengte,Breedte];
-            for (int i = 0; i < Lengte; i++)
+            klik_veld = new Button[bord.Lengte, bord.Breedte];
+            for (int i = 0; i < bord.Lengte; i++)
             {
-                for (int j = 0; j < Breedte; j++)
+                for (int j = 0; j < bord.Breedte; j++)
                 {
                     klik_veld[i,j] = new Button();
                     klik_veld[i,j].Location = new Point(i * 60, j * 60);
@@ -43,36 +38,20 @@ namespace Reversi
                     this.Controls.Add(klik_veld[i, j]);
                 }
             }
-            RenderBord(Bord, pea);
-        }
-
-        // Maakt een leeg bord met in het midden 4 stukken van beide partijen.
-        static int[,] start_bord(int Lengte,int Breedte)
-        {
-            int[,] start = new int[Lengte, Breedte];
-            for (int i = 0; i < Lengte; i++)
-            {
-                for (int j = 0; j < Breedte; j++)
-                    start[i, j] = 0;
-            }
-            start[Lengte / 2 - 1, Breedte / 2 - 1] = 1;
-            start[Lengte / 2, Breedte / 2] = 1;
-            start[Lengte / 2, Breedte / 2 - 1] = 2;
-            start[Lengte / 2 - 1, Breedte / 2] = 2;
-            return start;
+            RenderBord();
         }
 
         //De buttons in klik_veld worden geupdate met de waardes in bord 
-        void RenderBord(int[,] Bord, PaintEventArgs pea)
+        void RenderBord()
         {
-            Graphics gr = pea.Graphics;
+            // Graphics gr = pea.Graphics;
             Color[] kleur = { Color.White, Color.Blue, Color.Red };
-            SolidBrush[] steen = { new SolidBrush(Color.White), new SolidBrush(Color.Red), new SolidBrush(Color.Blue) };
-            for (int i = 0; i < Lengte; i++)
+            // SolidBrush[] steen = { new SolidBrush(Color.White), new SolidBrush(Color.Red), new SolidBrush(Color.Blue) };
+            for (int i = 0; i < bord.Lengte; i++)
             {
-                for (int j = 0; j < Breedte; j++)
+                for (int j = 0; j < bord.Breedte; j++)
                 {
-                    klik_veld[i, j].BackColor = kleur[Bord[i,j]];
+                    klik_veld[i, j].BackColor = kleur[bord.Velden[i,j]];
                     //gr.FillEllipse(steen[Bord[i, j]], i, j, 10, 10);
                 }
             }
@@ -82,7 +61,7 @@ namespace Reversi
         void Beurt(object sender, MouseEventArgs e)
         {
             Button b = (Button)sender;
-            if (Player % 2 != 0)
+            if (bord.Player % 2 != 0)
                 b.BackColor = Color.Red;
             // Add BLAUW!
 
@@ -91,14 +70,8 @@ namespace Reversi
             // Add ROOD!
 
             sender = (object)b;
-            Player += 1;
+            bord.Player += 1;
 
-        }
-
-        void BeurtValide()
-        {
-            // TODO kleur leeg
-            // TODO: Plek valide
         }
     }
 }
